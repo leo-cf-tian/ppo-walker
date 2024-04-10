202e161df27e9dc66c2967c7d8cf9c98a56eb651
leo-cf-tian <69664426+leo-cf-tian@users.noreply.github.com>
2024-04-10 07:51:52 -0700

    some optimizations

--- ppo.py
+++ ppo.py
@@ -3,6 +3,8 @@ from itertools import count
 import gym
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
+import os
+import random
 import time
 import torch
 from torch import nn, distributions, optim
@@ -23,16 +25,25 @@ class PPOAgent():
         self.critic_optim = optim.Adam(self.critic.parameters(), lr=self.lr)
         self.actor_optim = optim.Adam(self.actor.parameters(), lr=self.lr)
         
-    def set_hyperparams(self, batch_size=1000, learning_rate=0.005, grad_clip=5, discount_factor=0.99, covariance=0.5, clip=0.2):
+    def set_hyperparams(self, batch_size=2000, num_minibatch=3, updates_per_iteration=5, learning_rate=0.005, grad_clip=5, discount_factor=0.99, covariance=0.5, entropy_coefficient=0.0005, clip=0.2):
         """
         Sets hyperparameters of the agent
         """
         self.gamma = discount_factor
         self.covariance = torch.diag(torch.full((self.env.action_space.shape[0],), covariance))
         self.epsilon = clip
+        self.entropy_coefficient = entropy_coefficient
+        
         self.batch_size = batch_size
+        self.num_minibatch = num_minibatch
+        self.updates_per_iteration = updates_per_iteration
+        
         self.lr = learning_rate
         self.grad_clip = grad_clip
+        
+    def load_models(self, path):
+        self.critic.load_state_dict(torch.load(os.path.join(path, "critic.pt")))
+        self.actor.load_state_dict(torch.load(os.path.join(path, "actor.pt")))
         
     def collect_batch(self, render):
         """
@@ -45,17 +56,20 @@ class PPOAgent():
             trajectory: list[Trajectory] = []
         
             observation = self.env.reset()
-            observation = torch.tensor(observation, dtype=torch.float)
+            observation = torch.tensor(observation, dtype=torch.float32)
         
             for t in count():
                 action, log_prob = self.get_action(observation)
                 new_observation, reward, terminated, truncated = self.env.step(action)
+                
+                if not torch.is_tensor(reward):
+                    reward = torch.tensor(reward, dtype=torch.float32)
 
                 trajectory.append(Trajectory(observation, action, log_prob, reward, 0))
 
                 done = terminated or truncated
 
-                observation = torch.tensor(new_observation, dtype=torch.float).flatten()
+                observation = torch.tensor(new_observation, dtype=torch.float32).flatten()
 
                 if render and len(rollout) == 0:
                     self.env.render()
@@ -63,14 +77,14 @@ class PPOAgent():
                 if done:
                     break
             
-            returns = []
+            rewards = []
             reward_to_go = 0
             for timestep in reversed(trajectory):
-                reward_to_go = timestep.reward + reward_to_go * self.gamma
-                returns.append(timestep.reward)
+                reward_to_go = (timestep.reward + reward_to_go * self.gamma).to(torch.float32)
+                rewards.append(timestep.reward)
                 rollout.append(timestep._replace(reward_to_go=reward_to_go))
                 
-            episodic_return.append(sum(returns))
+            episodic_return.append(sum(rewards))
             
         return rollout, sum(episodic_return) / len(episodic_return)
     
@@ -92,21 +106,18 @@ class PPOAgent():
         
         return action.detach(), log_prob.detach()
     
-    def train(self, iterations=1000, updates_per_iteration=5, render=False):
+    def train(self, iterations=1000, render=False, render_threshold=0):
+        random.seed(time.time())
         
         avg_return = []
-        avg_actor_loss = []
-        avg_critic_loss = []
         timesteps = []
         
         plt.ion()
         figure, ax = plt.subplots()
         return_line, = ax.plot([], [], label="Return")
-        # actor_line, = ax.plot([], [], label="Actor Loss")
-        # critic_line, = ax.plot([], [], label="Critic Loss")
             
         for i in range(iterations):
-            rollout, mean_return = self.collect_batch(render and i > 400)
+            rollout, mean_return = self.collect_batch(render and i > render_threshold)
             rollout = Trajectory(*[torch.stack(list(t)) for t in zip(*rollout)])
             
             value = self.critic.forward(rollout.observation).squeeze()
@@ -116,41 +127,65 @@ class PPOAgent():
             actor_losses = []
             critic_losses = []
             
-            for j in range(updates_per_iteration):
-                actor_losses.append(self.update_actor(rollout, advantage))
-                critic_losses.append(self.update_critic(rollout))
+            indices = list(range(len(advantage)))
+            
+            for j in range(self.updates_per_iteration):
+                random.shuffle(indices)
+                for k in range(0, len(indices), len(indices) // self.num_minibatch):
+                    actor_losses.append(self.update_actor(rollout.observation, rollout.action, rollout.log_prob, advantage))
+                    critic_losses.append(self.update_critic(rollout.observation, rollout.reward_to_go))
                 
             avg_return.append(mean_return)
-            avg_actor_loss.append((sum(actor_losses) / len(actor_losses)).item())
-            avg_critic_loss.append((sum(critic_losses) / len(critic_losses)).item() / 5000)
             timesteps.append(len(rollout.reward) + timesteps[-1] if len(timesteps) > 0 else 0)
             
             return_line.set_xdata(timesteps)
             return_line.set_ydata(avg_return)
-            # actor_line.set_xdata(timesteps)
-            # actor_line.set_ydata(avg_actor_loss)
-            # critic_line.set_xdata(timesteps)
-            # critic_line.set_ydata(avg_critic_loss)
             ax.set_xlim([0, timesteps[-1]])
             ax.set_ylim([min(avg_return), max(avg_return)])
             ax.set_xlabel('Timesteps')
             ax.set_ylabel('Value')
             ax.legend()
+            ax.set_title(f"Iteration: {i + 1}")
             figure.canvas.draw()
             figure.canvas.flush_events()
                 
+    def run(self):
+        while True:
+            observation = self.env.reset()
+            observation = torch.tensor(observation, dtype=torch.float32)
+            
+            for t in count():
+                action, _ = self.get_action(observation)
+                observation, _, terminated, truncated = self.env.step(action)
+                observation = torch.tensor(observation, dtype=torch.float32).flatten()
+                    
+                self.env.render()
+                time.sleep(0.001)
+
+                if terminated or truncated:
+                    break
                           
-    def update_actor(self, rollout: Trajectory, advantage: torch.Tensor):
-        mean_action = self.actor.forward(rollout.observation)
+    def update_actor(
+        self,
+        observation: torch.Tensor,
+        action: torch.Tensor,
+        log_prob: torch.Tensor,
+        advantage: torch.Tensor
+    ):
+        mean_action = self.actor.forward(observation)
         dist = distributions.MultivariateNormal(mean_action, self.covariance)
-        log_prob_new = dist.log_prob(rollout.action)
+        log_prob_new = dist.log_prob(action)
         
-        likelihood_ratio = torch.exp(log_prob_new - rollout.log_prob)
+        likelihood_ratio = torch.exp(log_prob_new - log_prob)
         
         surrogate_advantage = likelihood_ratio * advantage
         clipped_surrogate_advantage = torch.clamp(likelihood_ratio, 1 - self.epsilon, 1 + self.epsilon) * advantage
         
         loss = (-torch.min(surrogate_advantage, clipped_surrogate_advantage).mean())
+
+        entropy_loss = -dist.entropy().mean() * self.entropy_coefficient
+        
+        loss = loss + entropy_loss
         
         self.actor_optim.zero_grad()
         loss.backward(retain_graph=True)
@@ -159,9 +194,13 @@ class PPOAgent():
         
         return loss
         
-    def update_critic(self, rollout: Trajectory):
-        value = self.critic.forward(rollout.observation)
-        loss = nn.MSELoss()(value, rollout.reward_to_go)
+    def update_critic(
+        self, 
+        observation: torch.Tensor,
+        reward_to_go: torch.Tensor
+    ):
+        value = self.critic.forward(observation).squeeze()
+        loss = nn.MSELoss()(value, reward_to_go)
     
         self.critic_optim.zero_grad()
         loss.backward()
@@ -176,11 +215,13 @@ class PolicyApproximator(nn.Module):
         super().__init__()
         
         self.model = nn.Sequential(
-            nn.Linear(n_observations, 64),
+            nn.Linear(n_observations, 128),
             nn.ReLU(),
-            nn.Linear(64, 64),
+            nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(64, n_actions),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, n_actions),
         )
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -191,19 +232,33 @@ class ValueApproximator(nn.Module):
         super().__init__()
         
         self.model = nn.Sequential(
-            nn.Linear(n_observations, 64),
+            nn.Linear(n_observations, 128),
             nn.ReLU(),
-            nn.Linear(64, 64),
+            nn.Linear(128, 128),
             nn.ReLU(),
-            nn.Linear(64, 1),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1),
         )
         
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.model(x)
 
 if __name__ == '__main__':
-    env = gym.make("Pendulum-v0")
+    BASE_PATH = "./models"
+    ENV_NAME = "BipedalWalkerHardcore-v3"
+    
+    pretrained = False
+    
+    env = gym.make(ENV_NAME)
     agent = PPOAgent(env)
-    agent.train(100000, 5, False)
-    torch.save(agent.actor.state_dict(), "./models/actor.pt")
-    torch.save(agent.critic.state_dict(), "./models/critic.pt")
+    
+    if (pretrained):
+        agent.load_models(os.path.join(BASE_PATH, ENV_NAME))
+    
+    agent.train(500, True, 490)
+    
+    torch.save(agent.actor.state_dict(), os.path.join(BASE_PATH, ENV_NAME, "actor.pt"))
+    torch.save(agent.critic.state_dict(), os.path.join(BASE_PATH, ENV_NAME, "critic.pt"))
+    
+    # agent.run()
